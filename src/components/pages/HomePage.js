7afe160a17af4b1c36e2a945081cd0adc6d3641e
awn_lunchon <awn@turnlunchon.com>
2017-10-24 12:36:56 +0400

    login logout completed

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logout } from '../../redux/actions/auth';
 
-const HomePage = () => (
+const HomePage = ({ isAuthenticated, logout }) => (
   <div>
     <h2>Home Page</h2>
-    <Link to="/login">Login</Link>
-    <Link to="/">Home</Link>
+    {!isAuthenticated ? (
+      <Link to="/login">Login</Link>
+    ) : (
+      <button onClick={() => logout()}>logout</button>
+    )}
   </div>
 );
-
-export default HomePage;
+HomePage.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+  logout: PropTypes.func.isRequired,
+};
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.user.token,
+  };
+}
+export default connect(mapStateToProps,{logout})(HomePage);
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+import {userLoggedIn} from './redux/actions/auth'
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import rootReducer from './redux/rootReducer';
@@ -14,11 +14,13 @@ import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(
-    applyMiddleware(thunk),
-  ),
+  composeWithDevTools(applyMiddleware(thunk)),
 );
 
+if (localStorage.bookwormToken) {
+  const user = { token: localStorage.bookwormToken };
+  store.dispatch(userLoggedIn(user));
+}
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
